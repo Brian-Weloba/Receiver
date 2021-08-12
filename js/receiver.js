b1c519f8764bf583c1c00edbb8963607d4b4ee52
Brian-Weloba <bweloba@gmail.com>
2021-08-12 16:30:08 +0300

    Add receiver debugger

--- js/receiver.js
+++ js/receiver.js
@@ -1,1 +1,15 @@
-cast.framework.CastReceiverContext.getInstance().start();
+const context = cast.framework.CastReceiverContext.getInstance();
+const playerManager = context.getPlayerManager();
+
+//Debug Logger
+const castDebugger = cast.debug.CastDebugLogger.getInstance();
+const LOG_TAG = 'MyApp.LOG';
+
+// Enable debug logger and show a 'DEBUG MODE' overlay at top left corner.
+castDebugLogger.setEnabled(true);
+
+// Set verbosity level for Core events.
+castDebugger.loggerLevelByEvents = {
+    'cast.framework.events.category.CORE': cast.framework.LoggerLevel.INFO,
+    'cast.framework.events.EventType.MEDIA_STATUS': cast.framework.LoggerLevel.DEBUG
+}
